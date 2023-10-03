44a89d4e86882cdaa756d57a0a58acba7b923dc6
JAKE MARK GALLARDEZ SALAZAR <146406096+JakeMark011@users.noreply.github.com>
2023-10-03 22:08:26 +0800

    Add files via upload

---
+++ SAMPLE 11.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+  
+    int x;
+    
+    cout << "Enter your Age:";
+    cin >> x;
+    
+    if(x>=18){
+     cout << "You are a Legal Age\n";
+     cout <<"Pwede Ka eon Makulong";
+     
+ }else{
+  
+    cout << "You are a minor\n";
+    cout <<"Bata ka Pa Boi";
+    
+    
+ }
+  
+  return 0;
+}
